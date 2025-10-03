2617541caced81ae37e0937fecf7301e718ebfd7
user <user@DESKTOP-EVSROCT>
2025-10-03 16:29:57 +0300

    validator fix

--- app/src/main/java/web2/Validator.java
+++ app/src/main/java/web2/Validator.java
@@ -6,13 +6,14 @@ public class Validator {
     private static BigDecimal bd(double d) {
         return BigDecimal.valueOf(d);
     }
+    private static final float MAX_R = 100f;
 
     public static boolean checkX(BigDecimal x){
-        return (x.compareTo(bd(-3)) >= 0) && (x.compareTo(bd(5)) <= 0);
+        return (x.compareTo(bd(-MAX_R)) >= 0) && (x.compareTo(bd(MAX_R)) <= 0);
     }
 
     public static boolean checkY(BigDecimal y){
-        return (y.compareTo(bd(-5)) >= 0) && (y.compareTo(bd(3)) <= 0);
+        return (y.compareTo(bd(-MAX_R)) >= 0) && (y.compareTo(bd(MAX_R)) <= 0);
     }
 
     public static boolean checkR(BigDecimal r){
